304946a13edbc7b8db8648954fa015b7ebafb85e
Jason Scholl <jason.e.scholl@gmail.com>
2018-06-21 07:35:32 -0700

    more wip

--- test/helper.sh
+++ test/helper.sh
@@ -1,4 +1,4 @@
-[[ -z "$SHUNIT2"     ]] && SHUNIT2=/usr/share/shunit2/shunit2
+[[ -z "$SHUNIT2"     ]] && SHUNIT2=$(command -v shunit2)
 #[[ -n "$ZSH_VERSION" ]] && setopt shwordsplit
 
 export PREFIX="$PWD/test"
